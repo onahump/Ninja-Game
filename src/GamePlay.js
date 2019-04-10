cb773cd543468b64d6394006833d9b12e5baf04c
Nahum Okysoft <nahum.poar@gmail.com>
2019-04-10 18:10:54 -0500

    Loading ninja image and making a tween animation with it

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -8,11 +8,20 @@ GamePlayManager = {
         game.load.image('background', 'assets/img/background.png');
         game.load.image('object1' , 'assets/img/objects1.png');
         game.load.image('object2' , 'assets/img/objects2.png');
+        game.load.image('ninja' , 'assets/img/ninja.png');
+
     },
     create: function(){
         game.add.sprite(0,0, 'background');
         game.add.sprite(0,100, 'object1');
         game.add.sprite(0,0, 'object2');
+
+        var ninja = game.add.sprite(500, 300, 'ninja');
+
+        this.tween = game.add.tween(ninja);
+        this.tween.to({x: 930, y:0}, 1500, Phaser.Easing.Quadratic.In);
+        this.tween.start();
+
     },
     update: function(){
     }
