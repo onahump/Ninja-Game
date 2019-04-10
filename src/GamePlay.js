be92a4ae4b6d57904701a05c56fdfee58ac4d073
Nahum Okysoft <nahum.poar@gmail.com>
2019-04-10 17:33:46 -0500

    Showing background image

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,16 +1,18 @@
 GamePlayManager = {
     init: function(){
-        
+
     },
     preload: function(){
+        game.load.image('background', 'assets/img/background.png');
     },
     create: function(){
+        game.add.sprite(0,0, 'background');
     },
     update: function(){
     }
 }
 
- var game =  new Phaser.Game(1012, 657, Phaser.CANVAS);
+var game =  new Phaser.Game(1012, 657, Phaser.CANVAS);
 
 game.state.add("gameplay", GamePlayManager);
 game.state.start("gameplay");
