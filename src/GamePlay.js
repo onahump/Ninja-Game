a7bcd7962c8bea9098b0d4d5871354dac1a16c42
Nahum Okysoft <nahum.poar@gmail.com>
2019-04-10 17:39:17 -0500

    Loading images and adding sprites

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,12 +1,15 @@
 GamePlayManager = {
     init: function(){
-
     },
     preload: function(){
         game.load.image('background', 'assets/img/background.png');
+        game.load.image('object1' , 'assets/img/objects1.png');
+        game.load.image('object2' , 'assets/img/objects2.png');
     },
     create: function(){
         game.add.sprite(0,0, 'background');
+        game.add.sprite(0,100, 'object1');
+        game.add.sprite(0,0, 'object2');
     },
     update: function(){
     }
