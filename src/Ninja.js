2548441b53bf6b227f434c9fcd19ad2eaff0f57d
Nahum Okysoft <nahum.poar@gmail.com>
2019-04-11 16:09:14 -0500

    Showing a message when we do click on ninja

--- src/Ninja.js
+++ src/Ninja.js
@@ -1,5 +1,12 @@
 Ninja = function () {
     Phaser.Sprite.call(this, game, 100,500, 'ninja');
+
+    this.inputEnabled = true; //this se refiere a nuestra funcion ninja. PErmite realizar click
+    this.input.pixelPerfectClick = true; //Solo permite dar click en la imagen, no en lo transparente
+    this.events.onInputDown.add( function(){
+        console.log("CLICK"); 
+    }, this);
+
 }
 Ninja.prototype = Object.create(Phaser.Sprite.prototype);
 Ninja.prototype.constructor = Ninja;
